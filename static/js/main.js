cad35eba86efa856ae438959dc72b29b53a6c05b
jktravis <joshua.travis@gmail.com>
2016-03-25 20:09:27 -0400

    Tweaked colors. Added audio. Added button press highlights.

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,36 @@
 $(document).ready(function () {
   var Simon = exports.simon;
-  var seqFunctions = [];
+  var $buttons = $('#simon > button');
+  var sounds = [
+    new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+    new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+    new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+    new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+  ];
+
   var colors = [
     {base: '#00c100', highlight: '#00ff00'},
-    {base: '#d20306', highlight: '#FD2C19'},
+    {base: '#d20306', highlight: '#FF4D52'},
     {base: '#CCCA4B', highlight: '#FFFF00'},
-    {base: '#0605C5', highlight: '#3A38FF'}
+    {base: '#0605C5', highlight: '#009CFF'}
   ];
 
   function playPattern() {
+    var seqFunctions = [];
     for (var i = 0; i < Simon.pattern.length; i++) {
       seqFunctions.push([flash, Simon.pattern[i]]);
     }
 
     seqFunctions.map(function(fun, index) {
-      setTimeout(fun[0], 500 * index, fun[1]);
+      setTimeout(fun[0], 1000 * index, fun[1]);
     });
   }
 
   function flash(id) {
     var $sel = $('#' + id);
     (function() {
-      $sel.animate({backgroundColor: colors[id].highlight}, 500);
+      $sel.animate({backgroundColor: colors[id].highlight}, 750);
+      sounds[id].play();
     })();
     (function() {
       $sel.animate({backgroundColor: colors[id].base}, 300);
@@ -31,7 +40,13 @@ $(document).ready(function () {
   Simon.pattern.push(Simon.getNextInPattern());
   playPattern();
 
-  $('#simon > div').on('click', function() {
+  $buttons.on('mousedown', function() {
+    $(this).animate({backgroundColor: colors[this.id].highlight}, 'fast');
+    sounds[this.id].play();
+  });
+
+  $buttons.on('mouseup', function () {
+    $(this).animate({backgroundColor: colors[this.id].base}, 'fast');
     // Selection correct?
     if (Simon.compareSelection(Simon.currentStep, parseInt(this.id))) {
       console.log('Correct selection');
