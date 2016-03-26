73c4beaacdfa028037a5bb2aedf0c687a424ad06
jktravis <joshua.travis@gmail.com>
2016-03-26 12:29:32 -0400

    Abstracted some steps into functions. Added some documentation.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   var Simon = exports.simon;
   var $buttons = $('#simon > button');
+  var $powerSwitch = $('#power');
+  var $errorFlash = $('#errorFlash');
 
   var colors = [
     {base: '#00c100', highlight: '#00ff00'},
@@ -9,6 +11,9 @@ $(document).ready(function () {
     {base: '#0605C5', highlight: '#009CFF'}
   ];
 
+  /**
+   * Play the pattern on screen.
+   */
   function playPattern() {
     var seqFunctions = [];
     setTimeout(
@@ -23,10 +28,18 @@ $(document).ready(function () {
       }), 2000 );
   }
 
+  /**
+   * Play the audio for the provided button ID.
+   * @param id - the ID of the button.
+   */
   function playSound(id) {
     $('#audio-' + id).trigger('play');
   }
 
+  /**
+   * Flash the button for the provided ID.
+   * @param id - the ID of the button.
+   */
   function flash(id) {
     var $sel = $('#' + id);
     (function() {
@@ -38,6 +51,12 @@ $(document).ready(function () {
     })();
   }
 
+  /**
+   * Zero pad the number by the width.
+   * @param n - the number.
+   * @param width - how many zeros to left pad.
+   * @returns {string} - the zero padded string.
+   */
   function zfill(n, width) {
     var tmp = '';
     while ((tmp + n).length < width) {
@@ -46,15 +65,74 @@ $(document).ready(function () {
     return tmp;
   }
 
+  /**
+   * Update the score display.
+   */
   function updateScore() {
-    $('#score').text(zfill(Simon.pattern.length - 1, 2));
+    if (Simon.pattern.length === 0) {
+      $('#score').text('--');
+    }
+    else {
+      $('#score').text(zfill(Simon.pattern.length - 1, 2));
+    }
   }
 
-  $("[name='my-checkbox']").bootstrapSwitch();
+  /**
+   * Turn on and setup the game.
+   */
+  function turnOn() {
+    $buttons.prop('disabled', false);
+    Simon.reset();
+    updateScore();
+    playPattern();
+  }
 
-  Simon.pattern.push(Simon.getNextInPattern());
-  updateScore();
-  playPattern();
+  /**
+   * Turn off the game.
+   */
+  function turnOff() {
+    Simon.currentStep = 0;
+    Simon.pattern = [];
+    updateScore();
+    $buttons.prop('disabled', true);
+  }
+
+  /**
+   * Flash the screen red twice and replays the pattern.
+   * @param time - time in milliseconds for the time of each flash.
+   */
+  function selectionError(time) {
+    setTimeout(function () {
+      $errorFlash.modal('show');
+      setTimeout(function () {
+        $errorFlash.modal('hide');
+        setTimeout(function () {
+          $errorFlash.modal('show');
+          setTimeout(function () {
+            $errorFlash.modal('hide');
+          }, time)
+        }, time)
+      }, time)
+    }, time);
+
+    console.log('Incorrect selection');
+    console.log(Simon.pattern);
+    Simon.currentStep = 0;
+    playPattern();
+  }
+
+  $buttons.prop('disabled', true);
+
+  $powerSwitch.bootstrapSwitch();
+
+  $powerSwitch.on('switchChange.bootstrapSwitch', function (event, state) {
+    if (state) {
+      turnOn();
+    }
+    else {
+      turnOff();
+    }
+  });
 
   $buttons.on('mousedown', function() {
     $(this).animate({backgroundColor: colors[this.id].highlight}, 'fast');
@@ -84,23 +162,7 @@ $(document).ready(function () {
     }
     else {
       // flash on error
-      setTimeout(function() {
-        $('#errorFlash').modal('show');
-        setTimeout(function() {
-          $('#errorFlash').modal('hide');
-          setTimeout(function () {
-            $('#errorFlash').modal('show');
-            setTimeout(function () {
-              $('#errorFlash').modal('hide');
-            }, 300)
-          }, 300)
-        }, 300)
-      }, 300);
-
-      console.log('Incorrect selection');
-      console.log(Simon.pattern);
-      Simon.currentStep = 0;
-      playPattern();
+      selectionError(250);
     }
   })
 });
